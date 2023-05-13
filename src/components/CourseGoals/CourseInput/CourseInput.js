ddd2685fb202e67cb2d995b47b51c47643a9ab9d
Karan <karansethia24@gmail.com>
2023-05-13 23:51:56 +0530

    adding css modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,8 +1,40 @@
 import React, { useState } from 'react';
+import styles from './CourseInput.module.css'
 
+// import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+// import './CourseInput.css';
 
+//? Styled Components
+// const FormControl = styled.div`
+//     margin: 0.5rem 0;
+
+//   & label {
+//     color: ${props => props.invalid ? 'red' : 'rgb(126, 126, 126)'};
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => props.invalid ? 'red' : 'rgb(126, 126, 126)'};
+//     background-color: ${props => props.invalid ? 'rgb(255, 199, 199)' : 'white'};
+//     border-radius: 5px;
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #d7d6d6;
+//     border-color: #4e4e4ec1;
+//   }
+// `;
+
+// React Methods
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true);
@@ -25,7 +57,8 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid? 'invalid':''}`}>
+      {/* One way of adding dynamic classes => <FormControl className={!isValid && 'invalid'}> */}
+      <div className={`${styles['form-control']} ${!isValid ? styles.invalid : ''}`}>
         <label >Today's Goal</label>
         <input type="text" style={{borderColor: !isValid ? 'red' : 'grey'}} onChange={goalInputChangeHandler}/>
       </div>
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,36 @@
-import React from 'react';
+// import styled from 'styled-components';
 
-import './Button.css';
+import styles from './Button.module.css'
+
+//? styled.button`` => this is called a tagged template literal
+// const Button = styled.button`
+//     font: inherit;
+//     padding: 0.5rem 1.5rem;
+//     border: 1px solid #4422dc;
+//     border-radius: 5px;
+//     color: white;
+//     background: #4422dc;
+//     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//     cursor: pointer;
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #26199c;
+//     border-color: #26199c;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+
+// `;  
+
+
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
